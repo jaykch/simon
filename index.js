79980e2868fbcced93b83dea28576959a595793d
Jay Kumar <jaykch@outlook.com>
2016-11-17 13:48:37 +0000

    Finished view and basic click functionality

--- index.js
+++ index.js
@@ -1,15 +1,119 @@
 /**
  * Created by Jay on 11/16/2016.
  */
-$(document).ready(function(){
+$(document).ready(function () {
+    game.init();
 });
 
-function playAudio(){
-    document.getElementById("audio-2").play();
-}
-
-function log(){
+function log() {
     console.log(arguments);
 }
 
-var App = (function(){})();
+var game = (function () {
+
+    var audioFiles = [];
+    var buttons = [];
+    var buttonAudioSpeed = 1.5;
+    var onClickOpacity = [1, 0.5];
+
+    var init = function () {
+        _getAudio();
+        _getButtons();
+        _audioHandler();
+        _render();
+    };
+
+    var _getAudio = function () {
+        for (var i = 0; i < 4; i++) {
+            audioFiles.push(document.getElementById("audio-" + i));
+        }
+    };
+
+    var _audioHandler = function () {
+        buttons[0].click(function () {
+            _playAudio(0);
+            _changeButtonOpacity(0);
+        });
+        buttons[1].click(function () {
+            _playAudio(1);
+            _changeButtonOpacity(1);
+        });
+        buttons[2].click(function () {
+            _playAudio(2);
+            _changeButtonOpacity(2);
+        });
+        buttons[3].click(function () {
+            _playAudio(3);
+            _changeButtonOpacity(3);
+        });
+    };
+
+    var _playAudio = function (audioId) {
+        switch (audioId) {
+            case 0:
+                audioFiles[0].playbackRate = buttonAudioSpeed;
+                audioFiles[0].play();
+                break;
+            case 1:
+                audioFiles[1].playbackRate = buttonAudioSpeed;
+                audioFiles[1].play();
+                break;
+            case 2:
+                audioFiles[2].playbackRate = buttonAudioSpeed;
+                audioFiles[2].play();
+                break;
+            case 3:
+                audioFiles[3].playbackRate = buttonAudioSpeed;
+                audioFiles[3].play();
+                break;
+        }
+    };
+
+    var _changeButtonOpacity = function (buttonId) {
+        switch (buttonId) {
+            case 0:
+                buttons[0].css("opacity", "1");
+                var interval0 = setInterval(function () {
+                    buttons[0].css("opacity", "0.5");
+                    clearInterval(interval0);
+                }, 500);
+                break;
+            case 1:
+                buttons[1].css("opacity", "1");
+                var interval1 = setInterval(function () {
+                    buttons[1].css("opacity", "0.5");
+                    clearInterval(interval1);
+                }, 500);
+                break;
+            case 2:
+                buttons[2].css("opacity", "1");
+                var interval2 = setInterval(function () {
+                    buttons[2].css("opacity", "0.5");
+                    clearInterval(interval2);
+                }, 500);
+                break;
+            case 3:
+                buttons[3].css("opacity", "1");
+                var interval3 = setInterval(function () {
+                    buttons[3].css("opacity", "0.5");
+                    clearInterval(interval3);
+                }, 500);
+                break;
+        }
+    };
+
+    var _getButtons = function () {
+        for (var i = 0; i < 4; i++) {
+            buttons.push($("#button-" + i));
+        }
+    };
+
+    var _render = function () {
+
+    };
+
+    return {
+        init: init
+
+    }
+})();
