e368e47a64164d75babebd5e5a8f92491629ed2f
Jay Kumar <jaykch@outlook.com>
2016-11-17 11:46:00 +0000

    Initial Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Jay on 11/16/2016.
+ */
+$(document).ready(function(){
+});
+
+function playAudio(){
+    document.getElementById("audio-2").play();
+}
+
+function log(){
+    console.log(arguments);
+}
+
+var App = (function(){})();
